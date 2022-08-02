6a8986a975d1d3086313001a74fae497b92e4e09
Dima <dimapetrov78959@gmail.com>
2022-08-02 15:11:04 +0300

    Finish

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,22 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemList} = item;
+        return (
+            <EmployeesListItem 
+            key={id} 
+            {...itemList} 
+            onDelete={() => onDelete(id)}
+            onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/>
+        )
+    })
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
